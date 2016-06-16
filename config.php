fe1f9e153cdc940abaf1a1e9731736c4360137a0
Baccs <revlv.jbaccs@gmail.com>
2016-06-16 14:01:11 +0800

    Clean Add and view class

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /**
+     * The default driver we will be using
+     */
+    'driver' => 'mysql',
+
+    'mysql' => [
+        'host'     => 'localhost',
+        'username' => 'root',
+        'password' => 'P@ssw0rd',
+        'database' => 'baccs',
+        'charset'  => 'utf8'
+    ]
+];
---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Trial-PDO</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+</head>
+<body>
+<div class='wrapper'>
+    <form name='baccs' method='post' action='index.php'>
+    Title: <br>
+    <input type='text' id='title' name='title' required='required' ><br>
+    Message: <br>
+    <input type='text' id='message' name='message' required='required' ><br>
+    <br>
+    <input type='submit' name='submit' value='submit'>
+    </form>
+</div>
+</body>
+</html>
+
+
+
+<?php
+$link = new PDO('mysql:host=localhost;dbname=baccs', "root", "P@ssw0rd");
+
+
+class AddInfo {
+    public function __construct($link) {
+
+    }
+
+    function addsubmit($title, $message) {
+        if(isset($_POST["submit"])){
+        try {
+        $link = new PDO('mysql:host=localhost;dbname=baccs', "root", "P@ssw0rd");
+        $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql="INSERT INTO `baccs` . `ToDo` (`title`, `message`)
+        VALUES ('$_POST[title]','$_POST[message]')";
+        if ($link->query($sql)) {
+        echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+        }
+        else{
+        echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+        }
+        $dbh = null;
+        }
+        catch(PDOException $e)
+        {
+        echo $e->getMessage();
+        }
+
+        }
+    }
+}
+
+
+class ViewInfo {
+    public function __construct($link) {
+
+    }
+    public function view ($link) {
+         $sql= "SELECT * FROM ToDo";
+                $stmt = $link->prepare($sql);
+                $stmt->execute();
+                $result = $stmt->fetchAll();
+                foreach($result as $row)
+                {
+                    echo "<div class='wrapper'>
+                    <form action='delete.php' method='POST' class='form-display'>";
+                    echo "<input type='text' name='id' value='{$row['id']}'>";
+                    echo "<input type='text' name='firstname' value='{$row['title']}'>";
+                    echo "<input type='text' name='lastname' value='{$row['message']}'>";
+                    echo "<span><a href='update.php'>Update</a></span>";
+                    echo "<span><a href='delete.php'>Delete</a></span>";
+                    echo "</form></div>";
+                }
+
+    }
+}
+
+
+class DeleteInfo {
+    public function __construct($link) {
+
+    }
+
+    public function deleterow ($link) {
+            $sql = "DELETE FROM ToDo WHERE id =  :id";
+            $stmt = $link->prepare($sql);
+            $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_INT);
+            $stmt->execute();
+            echo "<script type= 'text/javascript'>alert('Successfully Deleted.');</script>";
+            }
+
+    }
+
+
+
+$info = new AddInfo($link);
+$info->addsubmit($title, $message);
+$view = new ViewInfo();
+$view->view($link);
+$del = new DeleteInfo($link);
+$del->deleterow();
+
+?>
